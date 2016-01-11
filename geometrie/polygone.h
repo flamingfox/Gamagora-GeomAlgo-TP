0597305c9f2b5e79a6f3b30420970db1de822f83
RudiWelter <rudi.welter@laposte.net>
2016-01-11 15:47:40 +0100

    Polygone

---
+++ geometrie/polygone.h
@@ -0,0 +1,25 @@
+#ifndef POLYGONE_H
+#define POLYGONE_H
+
+#include <QVector>
+#include "vector2d.h"
+
+
+class Polygone
+{
+public:
+    Polygone();
+    inline bool isLinked(){return _linked;}
+    inline void setLinked(bool linked){_linked = linked;}
+    inline QVector<Vector2D> getPoints(){return _points;}
+
+
+
+protected:
+    QVector<Vector2D> _points;
+
+private:
+    bool _linked;
+};
+
+#endif // POLYGONE_H
