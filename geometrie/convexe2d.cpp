6f4ce2249fae93c36876ed7be73235ed7a31f4a5
aureargo <aureargo@hotmail.fr>
2016-01-11 16:34:09 +0100

    somme de Minkowski

--- geometrie/convexe2d.cpp
+++ geometrie/convexe2d.cpp
@@ -1,8 +1,8 @@
 #include "convexe2d.h"
 
-Convexe2D::Convexe2D(QVector<Vector2D> points)
+Convexe2D::Convexe2D(const QVector<Vector2D>& points):
+    Polygone(points)
 {
-
 }
 
 Convexe2D::Convexe2D(Vector2D point0, Vector2D point1, Vector2D point2)
@@ -14,7 +14,13 @@ Convexe2D::Convexe2D(Vector2D point0, Vector2D point1, Vector2D point2)
 
 Convexe2D Convexe2D::operator+(const Convexe2D& convexe) const
 {
-    return *this;
+    QVector<Vector2D> pointsAdd;
+    for(const Vector2D& p1 : this->_points)
+        for(const Vector2D& p2 : convexe._points)
+            pointsAdd.push_back(p1+p2);
+
+
+    return Convexe2D(pointsAdd);
 }
 
 Convexe2D Convexe2D::operator*(float scale) const
