1dbe1d7f34fb54b973dad2caac32d3c4ec16acc0
flamingfox <julien.brillet@yahoo.fr>
2016-01-20 17:22:13 +0100

    ça marche avec vol d'oiseau

--- TP2_B_skeleton/main.cpp
+++ TP2_B_skeleton/main.cpp
@@ -11,23 +11,27 @@
 int main(int argc, char *argv[])
 {
     QImage img("map.png");
-    TerrainTab terrain = TerrainTab(img, 1000, 1000);
+    TerrainTab terrain = TerrainTab(img, 1000, 1000, 100);
     QImage res = img.scaled(1000,1000);
+    QImage res2 = img.scaled(1000,1000);
+    QImage res3 = img.scaled(1000,1000);
 
     QPainter paint(&res);
 
+
     bSkeleton bS;
 
-    int nb = 100;
+    int nb = 50;
     for(int i=0; i < nb; i++){
         Vector2D pos((rand()%1001),(rand()%1001));
         Ville ville(Vector3D(XY(pos), terrain.getHauteur(XY(pos))));
 
+        //Ville ville(Vector3D(XY(pos), 0 ));
+
         bS.addVille(ville);
     }
 
-    bS.relierVille(2);
-
+    bS.relierVille(1);
 
     QPen pen(Qt::red);
     pen.setWidthF(1);
@@ -50,7 +54,57 @@ int main(int argc, char *argv[])
         paint.drawPoint(XY(v.getPosition()));
     }
     paint.end();
-    res.save("resultat.png");
+    res.save("resultat1.png");
+
+    /*========================================================*/
+
+    QPainter paint2(&res2);
+    bS.relierVille(2);
+
+    paint2.setPen(pen);
+    //paint.setBrush(QColor(255, 0, 0));
+
+    for(const Route& r : bS.getRoutes())
+    {
+        const Ville& v1 = r._ville1;
+        const Ville& v2 = r._ville2;
+        paint2.drawLine(XY(v1.getPosition()), XY(v2.getPosition()));
+    }
+
+    paint2.setPen(pen2);
+    //paint.setBrush(QColor(0, 255, 0));
+    for(const Ville& v: bS.getVilles())
+    {
+        paint2.drawPoint(XY(v.getPosition()));
+    }
+    paint2.end();
+    res2.save("resultat2.png");
+
+    /*========================================================*/
+
+
+    QPainter paint3(&res3);
+    bS.relierVille(8);
+
+    paint3.setPen(pen);
+    //paint.setBrush(QColor(255, 0, 0));
+
+    for(const Route& r : bS.getRoutes())
+    {
+        const Ville& v1 = r._ville1;
+        const Ville& v2 = r._ville2;
+        paint3.drawLine(XY(v1.getPosition()), XY(v2.getPosition()));
+    }
+
+
+    paint3.setPen(pen2);
+    //paint.setBrush(QColor(0, 255, 0));
+    for(const Ville& v: bS.getVilles())
+    {
+        paint3.drawPoint(XY(v.getPosition()));
+    }
+    paint3.end();
+    res3.save("resultat8.png");
 
     return 0;
 }
--- TP2_B_skeleton/math/bskeleton.cpp
+++ TP2_B_skeleton/math/bskeleton.cpp
@@ -4,20 +4,26 @@ bSkeleton::bSkeleton(){}
 
 bool bSkeleton::relierVille(const float &gamma)
 {
-    Vector3D a, b, p;
+    routes.clear();
 
+    Vector3D a, b, p;
+    bool creationOK;
     for(int i=0; i<villes.size(); i++){
         a = villes[i].getPosition();
+
         for(int j=i+1; j<villes.size(); j++){
             b = villes[j].getPosition();
+            creationOK = true;
+
             for(int k=0; k<villes.size(); k++){
                 if(k!=i && k!=j){
                     p = villes[k].getPosition();
-                    if(powf(distance(a, b), gamma) < powf(distance(a, p), gamma) + powf(distance(b, p), gamma)){
-                        routes.push_back(Route(villes[i], villes[j]));
-                    }
+                    creationOK =  (powf(distance(a, b), gamma) > (powf(distance(a, p), gamma) + powf(distance(b, p), gamma)))? false : creationOK;
+
                 }
             }
+            if(creationOK)
+                routes.push_back(Route(villes[i], villes[j]));
         }
     }
 }
